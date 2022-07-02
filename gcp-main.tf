80980fa869bd8bea6cffdec42ec04bc8f71d99af
marcellhkr <marcellhkr@gmail.com>
2022-07-02 11:33:13 -0300

    criacao banco

---
+++ gcp-main.tf
@@ -0,0 +1,25 @@
+resource "google_sql_database" "database" {
+  name     = "playlist"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "playlist-instance"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+   provisioner "local-exec" {
+    command = "PGPASSWORD=123456 psql -f playlist.sql -p 3306 -U playuser playlist-instance"
+  }
+  root_password = "123456"
+  deletion_protection  = "false"
+}
+
+resource "google_sql_user" "users" {
+  name     = "playuser"
+  instance = playlist-instance
+  password = "123456"
+}
