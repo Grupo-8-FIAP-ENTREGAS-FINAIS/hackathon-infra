22d6193fa0f937aed1a931ae3bf57d37bdbcbd42
marcellhkr <marcellhkr@gmail.com>
2022-07-02 12:43:56 -0300

    change db name

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,10 +1,10 @@
 resource "google_sql_database" "database" {
-  name     = "playlist"
+  name     = "playlist2"
   instance = google_sql_database_instance.playlist.name
 }
 
 # See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
-resource "google_sql_database_instance" "playlist" {
+resource "google_sql_database_instance" "playlist2" {
   name             = "playlist-instance"
   region           = "us-central1"
   database_version = "MYSQL_8_0"
